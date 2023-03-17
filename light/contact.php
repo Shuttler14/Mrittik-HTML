3e25ed13fddb65419bc08637c74ecc260c0f5e9b
Aryan <aryan.211me310@nitk.edu.in>
2023-03-17 13:38:06 +0530

    All done except 2

--- light/contact.php
+++ light/contact.php
@@ -1,8 +1,8 @@
-<?php $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+<?php $name = $_POST['HRO Marbite'];
+    $email = $_POST['hromarbite@gmail.com'];
+    $message = $_POST['Hello this mail is from HRO Marnites'];
     // $subject = $_POST['subject'];
-    $phone = $_POST['phone'];
+    $phone = $_POST['+84 964170063'];
     // $formcontent="From: $name \n Subject: $subject \n Message: $message \n Phone: $phone";
     $formcontent="From: $name \n Message: $message \n Phone: $phone";
     $recipient = "recipientmail@gmail.com";
